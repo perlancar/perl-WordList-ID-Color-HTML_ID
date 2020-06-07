33ec73c59d2927833e8c1211a972514144fb3906
perlancar (on pc-home) <perlancar@gmail.com>
2020-06-07 09:21:50 +0700

    Initial commit

---
+++ lib/WordList/ID/Color/HTML_ID.pm
@@ -0,0 +1,41 @@
+package WordList::ID::Color::HTML_ID;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use WordList;
+our @ISA = qw(WordList);
+
+# STATS
+
+1;
+# ABSTRACT: List of color names from Graphics::ColorNames::HTML_ID
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+Graphics::ColorNames::HTML_ID
+
+=cut
+
+__DATA__
+abu
+akua
+aqua
+biru
+birutua
+fuchsia
+hijau
+hitam
+kapur
+kuning
+merah
+merahmarun
+perak
+putih
+teal
+ungu
+zaitun
